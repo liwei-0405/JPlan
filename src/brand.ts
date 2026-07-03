18b7268a8d906846dc18e9c2fecffc2a3a072e30
Teh Li Wei <cha040405@gmail.com>
2026-07-04 06:37:17 +0800

    Rebuild responsive Calendar Status card

--- src/brand.ts
+++ src/brand.ts
@@ -1,3 +1,3 @@
-export const FRONTEND_VERSION = "2026.06.29-1";
+export const FRONTEND_VERSION = "2026.7.4-1";
 export const APP_VERSION = `v${FRONTEND_VERSION}`;
 export const jplanLogoUrl = "/logo.png";
